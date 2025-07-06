702bb6f5ec20041d6fd22311fe28ee11ed234eef
tskoyo <jovansr@protonmail.com>
2025-07-06 12:51:17 +0200

    Add mysql pool

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 use crate::models::Epic;
+use mysql::prelude::*;
+use mysql::*;
 
 mod models;
 
-fn main() {
-    let epic = Epic::new(
-        "API design".to_owned(),
-        "We should create a new API design".to_owned(),
-    );
+#[derive(Debug)]
+struct EpicRow {
+    id: i32,
+    name: String,
+    description: String,
+    status: String,
+}
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let url = "mysql://root:12345@192.168.0.63:3306/jira_clone";
+    let pool = Pool::new(url)?;
+
+    let mut conn = pool.get_conn()?;
+    let rows: Vec<EpicRow> = conn.query_map(
+        "SELECT id, name, description, status FROM epics",
+        |(id, name, description, status)| EpicRow {
+            id,
+            name,
+            description,
+            status,
+        },
+    )?;
+
+    for row in rows {
+        println!("{:?}", row);
+    }
+
+    Ok(())
 }
