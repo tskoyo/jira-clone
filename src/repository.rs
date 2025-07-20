d78d5c622a9ff312e31e93188a37f5022b3071d0
tskoyo <jovansr@protonmail.com>
2025-07-20 17:55:02 +0200

    Remove unused method

--- src/repository.rs
+++ src/repository.rs
@@ -13,43 +13,22 @@ impl<'a> Repository<'a> {
     pub async fn get_epic(&self, epic_id: u64) -> Result<Vec<EpicJsonRow>, sqlx::Error> {
         let rows = sqlx::query_as::<_, EpicJsonRow>(
             r#"
-        SELECT
-            e.name as epic_name,
-            e.description as epic_description,
-            e.status as epic_status,
-            s.name as story_name,
-            s.description as story_description,
-            s.status as story_status
-        FROM epics e
-        LEFT JOIN stories s ON e.id = s.epic_id
-        WHERE e.id = ?
-        "#,
+		SELECT
+			e.name as epic_name,
+			e.description as epic_description,
+			e.status as epic_status,
+			s.name as story_name,
+			s.description as story_description,
+			s.status as story_status
+		FROM epics e
+		LEFT JOIN stories s ON e.id = s.epic_id
+		WHERE e.id = ?
+		"#,
         )
         .bind(epic_id)
         .fetch_all(self.pool)
         .await?;
 
         Ok(rows)
-    }
-
-    pub async fn insert_epic(
-        &self,
-        name: &str,
-        description: &str,
-        status: &str,
-    ) -> Result<u64, sqlx::Error> {
-        let result = sqlx::query!(
-            r#"
-            INSERT INTO epics (name, description, status)
-            VALUES (?, ?, ?)
-            "#,
-            name,
-            description,
-            status
-        );
-
-        Ok(2)
-        // .execute(self.pool)
-        // .await?;
     }
 }
