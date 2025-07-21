8bbcfd39ddbcff93cb4f55b43f4119c323e6c80e
tskoyo <jovansr@protonmail.com>
2025-07-21 18:49:41 +0200

    Add new models

--- src/db.rs
+++ src/db.rs
@@ -1,37 +1,129 @@
-// trait Database {
-//     fn read_db();
-//     fn write_db();
-// }
+use anyhow::Result;
 
-pub struct DatabaseConnection {
-    pub username: String,
-    pub password: String,
-    pub address: String,
-    pub port: String,
-    pub db_name: String,
+use crate::models::{DBState, Epic, Status, Story};
+
+trait Database {
+    fn read_db(&self) -> Result<DBState>;
+    fn write_db(&self, db_state: &DBState) -> Result<()>;
 }
 
-impl DatabaseConnection {
-    pub fn new(
-        username: String,
-        password: String,
-        address: String,
-        port: String,
-        db_name: String,
-    ) -> Self {
-        DatabaseConnection {
-            username,
-            password,
-            address,
-            port,
-            db_name,
-        }
+struct JSONFileDatabase {
+    pub file_path: String,
+}
+
+impl Database for JSONFileDatabase {
+    fn read_db(&self) -> Result<DBState> {
+        todo!() // read the content's of self.file_path and deserialize it using serde
     }
 
-    pub fn format_url(&self) -> String {
-        format!(
-            "mysql://{}:{}@{}:{}/{}",
-            self.username, self.password, self.address, self.port, self.db_name
-        )
+    fn write_db(&self, db_state: &DBState) -> Result<()> {
+        todo!() // serialize db_state to json and store it in self.file_path
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod database {
+        use std::collections::HashMap;
+        use std::io::Write;
+
+        use super::*;
+
+        #[test]
+        fn read_db_should_fail_with_invalid_path() {
+            let db = JSONFileDatabase {
+                file_path: "INVALID_PATH".to_owned(),
+            };
+            assert_eq!(db.read_db().is_err(), true);
+        }
+
+        #[test]
+        fn read_db_should_fail_with_invalid_json() {
+            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+
+            let file_contents = r#"{ "last_item_id": 0 epics: {} stories {} }"#;
+            write!(tmpfile, "{}", file_contents).unwrap();
+
+            let db = JSONFileDatabase {
+                file_path: tmpfile
+                    .path()
+                    .to_str()
+                    .expect("failed to convert tmpfile path to str")
+                    .to_string(),
+            };
+
+            let result = db.read_db();
+
+            assert_eq!(result.is_err(), true);
+        }
+
+        #[test]
+        fn read_db_should_parse_json_file() {
+            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+
+            let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
+            write!(tmpfile, "{}", file_contents).unwrap();
+
+            let db = JSONFileDatabase {
+                file_path: tmpfile
+                    .path()
+                    .to_str()
+                    .expect("failed to convert tmpfile path to str")
+                    .to_string(),
+            };
+
+            let result = db.read_db();
+
+            assert_eq!(result.is_ok(), true);
+        }
+
+        #[test]
+        fn write_db_should_work() {
+            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+
+            let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
+            write!(tmpfile, "{}", file_contents).unwrap();
+
+            let db = JSONFileDatabase {
+                file_path: tmpfile
+                    .path()
+                    .to_str()
+                    .expect("failed to convert tmpfile path to str")
+                    .to_string(),
+            };
+
+            let story = Story {
+                name: "epic 1".to_owned(),
+                description: "epic 1".to_owned(),
+                status: Status::Open,
+            };
+            let epic = Epic {
+                name: "epic 1".to_owned(),
+                description: "epic 1".to_owned(),
+                status: Status::Open,
+                stories: vec![2],
+            };
+
+            let mut stories = HashMap::new();
+            stories.insert(2, story);
+
+            let mut epics = HashMap::new();
+            epics.insert(1, epic);
+
+            let state = DBState {
+                last_item_id: 2,
+                epics,
+                stories,
+            };
+
+            let write_result = db.write_db(&state);
+            let read_result = db.read_db().unwrap();
+
+            assert_eq!(write_result.is_ok(), true);
+            // TODO: fix this error by deriving the appropriate traits for DBState
+            assert_eq!(read_result, state);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,6 @@
-use crate::{db::DatabaseConnection, models::EpicJsonRow, repository::Repository};
-use sqlx::mysql::MySqlPool;
-
 mod db;
 mod models;
-mod repository;
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    let db_conection = DatabaseConnection::new(
-        "tskoyo".to_owned(),
-        "12345".to_owned(),
-        "192.168.0.63".to_owned(),
-        "3306".to_owned(),
-        "jira_clone".to_owned(),
-    );
-
-    let pool = MySqlPool::connect(&db_conection.format_url()).await?;
-    let repository = Repository::new(&pool);
-    let epic = repository.get_epic(2).await?;
-
-    for e in &epic {
-        println!("-------------------------");
-        println!("[Epic] name: {}", e.epic_name);
-        println!("Story name: {}", e.story_name);
-        match &e.story_description {
-            Some(desc) => println!("Description: {}", desc),
-            None => println!("Description: <none>"),
-        }
-        match &e.story_status {
-            Some(status) => println!("Status: {:?}", status),
-            None => println!("Status: <none>"),
-        }
-    }
-
-    Ok(())
+fn main() {
+    println!("Welcome To My-Jira!");
 }
